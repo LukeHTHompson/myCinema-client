a26c35727c4eb56d77aacbb70af28fca93db692f
LukeHTHompson <lht14dv@icloud.com>
2021-09-20 15:12:37 -0400

    3 View Setup (MainView, MovieCard, MovieView)

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,31 +1,35 @@
-import React from 'react';
+import React from "react";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
 
 class MainView extends React.Component {
 
   constructor() {
     super();
     this.state = {
-      movies: [
-        { _id: 1, Title: "Inception", Description: "test1", ImagePath: "..." },
-        { _id: 1, Title: "Inception", Description: "test1", ImagePath: "..." },
-        { _id: 1, Title: "Inception", Description: "test1", ImagePath: "..." }
-      ]
-    }
+      movies: [{ _id: 1, Title: "Inception", Description: "summary 1", ImagePath: "..." }, { _id: 2, Title: "Catch Me If You Can", Description: "summary 2", ImagePath: "..." }, { _id: 3, Title: "Gladiator", Description: "summary 3", ImagePath: "..." }],
+      selectedMovie: null
+    };
+  }
+
+  setSelectedMovie(newSelectedMovie) {
+    this.setState({
+      selectedMovie: newSelectedMovie
+    });
   }
 
   render() {
-    const movies = this.state.movies;
-    if (movies.length === 0) {
-      return <div className="main-view">The list is empty!</div>;
-    } else {
-      return (
-        <div className="main-view">
-          {movies.map((movie) => {
-            return <div>{movie.Title}</div>;
-          })}
-        </div>
-      );
-    }
+    const { movies, selectedMovie } = this.state;
+
+    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+
+    if (selectedMovie) return <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />;
+
+    return (
+      <div className="main-view">
+        {movies.map(movie => <MovieCard key={movie._id} movieData={movie} onMovieClick={(movieData) => { this.setSelectedMovie(movieData) }} />)}
+      </div>
+    );
   }
 }
 
