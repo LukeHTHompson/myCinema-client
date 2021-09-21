a0bf88ebaea29f8fb9dfb18408291e8a840c4537
LukeHTHompson <lht14dv@icloud.com>
2021-09-21 14:50:37 -0400

    PropType Definition for Movie Data

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+export function LoginView(props) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+    e.preventDefault();
+    console.log(username, password);
+    /* send request for auth */
+    props.onLoggedIn(username);
+  };
+
+  return (
+    <form>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>
+        Submit
+      </button>
+    </form>
+  );
+}
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export class MovieCard extends React.Component {
   render() {
     const { movieData, onMovieClick } = this.props;
-    return <div className="movie-card" onClick={() => { onMovieClick(movieData); }}> {movieData.Title} </div>;
+
+    return (
+      <div className="movie-card" onClick={() => { onMovieClick(movieData); }}> {movieData.Title} </div>
+    );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Imagepath: PropTypes.string.isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
