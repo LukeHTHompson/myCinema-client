b22c2bfb704abfe8be9bfe02cab0e64a7da2dac7
LukeHTHompson <lht14dv@icloud.com>
2021-09-23 13:51:53 -0400

    New Lines for Readability

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -21,18 +21,23 @@ export class MovieView extends React.Component {
 
     return (
       <div className="movie-view">
+
         <div className="movie-poster">
           <img src={movieData.ImagePath} />
         </div>
+
         <div className="movie-title">
           <span className="label">Title: </span>
           <span className="value">{movieData.Title}</span>
         </div>
+
         <div className="movie-description">
           <span className="label">Description: </span>
           <span className="value">{movieData.Description}</span>
         </div>
+
         <button onClick={() => { onBackClick(null); }}>Back</button>
+
       </div >
     );
   }
