290baa8bb62153e97473fed09bc56564d418a2fe
LukeHTHompson <lht14dv@icloud.com>
2021-11-04 20:44:34 -0400

    Add action/reducer for setting token as a global state

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,11 +1,20 @@
 // src/reducers/reducers.js
 import { combineReducers } from "redux";
 
-import { SET_USER, SET_MOVIES, SET_FILTER } from "../actions/actions";
+import { SET_USER, SET_TOKEN, SET_MOVIES, SET_FILTER } from "../actions/actions";
 
 function user(state = "", action) {
   switch (action.type) {
     case SET_USER:
+      return action.value;
+    default:
+      return state;
+  }
+}
+
+function token(state = "", action) {
+  switch (action.type) {
+    case SET_TOKEN:
       return action.value;
     default:
       return state;
@@ -33,6 +42,7 @@ function visibilityFilter(state = "", action) {
 function moviesApp(state = {}, action) {
   return {
     user: user(state.user, action),
+    token: token(state.token, action),
     movies: movies(state.movies, action),
     visibilityFilter: visibilityFilter(state.visibilityFilter, action),
   }
